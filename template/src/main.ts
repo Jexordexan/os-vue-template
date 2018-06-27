6a012f2439ffe26c55d3ba93f54061a47af43589
A Jordan Simonds <jsimonds@onshape.com>
2018-06-27 10:39:23 -0400

    Add vue-rx

--- template/src/main.ts
+++ template/src/main.ts
@@ -4,6 +4,15 @@ import App from 'App.vue';
 import SvgIcon from 'components/SvgIcon';
 import FocusDirective from 'directives/focus-directive';
 import Vue from 'vue';
+import VueRx from 'vue-rx';   // tslint:disable-line
+
+import { Observable, Subject, Subscription } from 'rxjs';
+
+Vue.use(VueRx, {
+  Observable,
+  Subject,
+  Subscription
+});
 
 Vue.component('SvgIcon', <any>SvgIcon); // tslint:disable-line:no-any
 Vue.directive('focus', FocusDirective);
